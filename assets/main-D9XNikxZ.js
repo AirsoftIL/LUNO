9db7099821dc64c212117b953d762cbdccf8533e
AirsoftIl <yakirelbaz55@gmail.com>
2026-09-17 14:47:42 +0300

    Finalize mobile film carousel and smart-glasses comparison

---
+++ assets/main-D9XNikxZ.js
@@ -0,0 +1,1 @@
+import{S as e,k as t,n}from"./product-Ct0HGLGF.js";var r=t(),i=e(),a=document.getElementById(`root`);if(!a)throw Error(`Missing root element`);(0,r.createRoot)(a).render((0,i.jsx)(n,{staticSite:!0,assetBase:`.`}));
